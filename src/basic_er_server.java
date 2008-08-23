7b605fec1fc525a7898515adec8a38d319f3dab5
neskataguh <neskataguh>
2008-08-23 02:51:51 +0000

    cleanup

--- src/basic_er_server.java
+++ src/basic_er_server.java
@@ -1,29 +1,29 @@
 /* basic_er_server.java */
 
-
 import net.lupulin.net.EventRingServer;
 import net.lupulin.event.EventRing;
 import net.lupulin.event.MidiEventGenerator;
 
-public class  basic_er_server {
-    public static void main(String[] args ){
+public class  basic_er_server 
+{
+    public static void main(String[] args)
+    {
         EventRing er = new EventRing();
         EventRingServer ers = new EventRingServer(60061);        
         MidiEventGenerator meg = new MidiEventGenerator();
 
         ers.set_ER( er );
 
-        for( int i=0; i< 999; i++ ){
+        for(int i=0; i< 999; i++){
             er.add( meg.getRandMidiEvent() );
         }
 
-        for( int i=0; i<er.size(); i++){
-          System.out.println( er.get(i).toString() );
+        for(int i=0; i<er.size(); i++){
+        	System.out.println( er.get(i).toString() );
         }
 
         ers.setup_server();
         ers.start_server();
-        
     }
 }   
 
